893576bd81eacf9ed1c845c6262ffc0cb34722fd
John Ramsden <john@ubc.ramsdenj.ca>
2025-05-21 14:07:08 -0700

    nvme crate should never expose unsafe

--- oxcache/src/main.rs
+++ oxcache/src/main.rs
@@ -1,6 +1,6 @@
 use nvme;
 
 fn main() {
-	let fd = unsafe{nvme::nvme_open("/dev/nvme1n1".as_ptr())};
-	println!("fd: {}", fd);
+	// let fd = unsafe{nvme::nvme_open("/dev/nvme1n1".as_ptr())};
+	// println!("fd: {}", fd);
 }
