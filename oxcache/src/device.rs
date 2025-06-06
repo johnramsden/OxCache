823d444efb5e8b3ae6fdb66bd4953e41dcac4b88
John Ramsden <john@ubc.ramsdenj.ca>
2025-06-06 13:52:36 -0700

    Update appent interface

--- oxcache/src/device.rs
+++ oxcache/src/device.rs
@@ -1,7 +1,27 @@
-struct Zoned {}
+use std::os::fd::RawFd;
+
+struct Zoned {
+    fd: RawFd,
+    timeout: u32,
+    logical_block_size: usize,
+    nsid: u32, // Can we set once?
+    zone_size: u64,
+}
 
 struct BlockInterface {}
 
 trait Device {
-    fn append() -> String; // TODO: Return value
+    fn append(data: Vec<u8>, zone: u64) -> tokio::io::Result<u64>;
+}
+
+impl Device for Zoned {
+    fn append(data: Vec<u8>, zone: u64) -> std::io::Result<u64> {
+        Ok(0)
+    }
+}
+
+impl Device for BlockInterface {
+    fn append(data: Vec<u8>, zone: u64) -> std::io::Result<u64> {
+        Ok(0)
+    }
 }
