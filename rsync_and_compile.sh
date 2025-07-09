b4f4199ab90376c97eb065c005c3496a0d19485b
slc <samc5670@proton.me>
2025-07-08 17:13:23 -0700

    update configuration

--- rsync_and_compile.sh
+++ rsync_and_compile.sh
@@ -10,5 +10,14 @@ rsync -avP \
 sshpass -p "ubuntu" ssh -o StrictHostKeyChecking=no -p 2222 ubuntu@127.0.0.1 << 'EOF'
 cd /home/ubuntu/OxCacheLocal/
 cargo build
+cargo run --bin oxcache -- --config example.server.toml &
+SERVER_PID=$!
+sleep 5
+cargo run --bin client -- --socket /tmp/oxcache.sock
+
+kill -9 $SERVER_PID
+kill -9 $CLIENT_PID
+
+trap 'finish' INT
 EOF
 
