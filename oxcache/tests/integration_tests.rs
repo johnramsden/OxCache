717fde3ece2383d3c970c2ce8ad342cc1a93ef40
slc <samc5670@proton.me>
2025-07-31 00:20:56 -0700

    add cli argument parsing to test
    unfortunately we can't pass arguments into integration tests, so we
    have to specify the config in the test.

--- oxcache/tests/integration_tests.rs
+++ oxcache/tests/integration_tests.rs
@@ -1,6 +1,12 @@
 use clap::Parser;
-use oxcache::cli::CliArgs;
+use oxcache::cli::{load_config, CliArgs};
 
 fn main() {
-    let _cli = CliArgs::parse();
+    let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("../example.server.toml");
+
+    let path_str = path.to_str().unwrap();
+    let cli = CliArgs::parse_from(["oxcache", "--config", path_str]);
+    let config = load_config(&cli).unwrap();
+    println!("{:?}", config);
 }
