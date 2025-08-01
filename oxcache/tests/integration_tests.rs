4d46f3abc31ca2e794226293c82ba02f73cd8e1d
slc <samc5670@proton.me>
2025-07-31 18:54:53 -0700

    try to set up github runner

--- oxcache/tests/integration_tests.rs
+++ oxcache/tests/integration_tests.rs
@@ -2,11 +2,17 @@ use clap::Parser;
 use oxcache::cli::{load_config, CliArgs};
 
 fn main() {
-    let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("../example.server.toml");
+    // Change based on if we are running in GH actions or not
+    let path = match option_env!("RUNNING_IN_GITHUB_ACTIONS") {
+        Some(_) => std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("../github_runner.server.toml"),
+        None => std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("../example.server.toml")
+    };
 
     let path_str = path.to_str().unwrap();
     let cli = CliArgs::parse_from(["oxcache", "--config", path_str]);
     let config = load_config(&cli).unwrap();
-    println!("{:?}", config);
+
 }
+
